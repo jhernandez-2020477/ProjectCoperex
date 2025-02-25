3a8142ee52a2b747bf109571d89b2cef05c675aa
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-02-24 23:05:27 -0600

    Se agrega rutas de autenticación para Empresa

--- src/company/company.routes.js
+++ src/company/company.routes.js
@@ -1,1 +1,30 @@
 //Rutas de autenticación
+import { Router } from "express";
+import { saveCompany, updateCompany } from "./company.controller.js";
+import { isAdmin, validateJwt } from "../../middlewares/validate.jwt.js"
+import { validSaveCompany, validUpdateCompany  } from "../../helpers/validators.js";
+
+const api = Router()
+
+//Agregar Publicación
+api.post(
+    '/saveCompany',
+    [
+        validSaveCompany
+    ],
+    saveCompany
+)
+
+//Actualizar Company
+api.put(
+    '/:id',
+    [
+        validateJwt,
+        isAdmin,
+        validUpdateCompany
+    ],
+    updateCompany
+)
+
+//Exportar 
+export default api
