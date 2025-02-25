27c7da558f26cd0d3c1260f2cec80c3f22b074f0
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-02-24 23:03:12 -0600

    Se agrega rutas de autenticación para registrar y hacer login

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,1 +1,32 @@
 //Rutas de auntenticación
+import { Router } from "express";
+import { register, test, login } from "./auth.controller.js";
+import { loginValidation, registerValidation } from '../../helpers/validators.js'
+import { uploadProfilePicture } from '../../middlewares/multer.uploads.js'
+import { deleteFileOnError } from '../../middlewares/delete.file.on.error.js'
+
+
+const api = Router()
+//Rutas públicas
+api.get('/test', test)
+
+api.post(
+    '/register', 
+    [
+        uploadProfilePicture.single('profilePicture'), 
+        registerValidation, 
+        deleteFileOnError
+    ],
+        register
+)
+
+api.post(
+    '/login', 
+    [
+        loginValidation
+    ], 
+    login
+)
+
+//Exportar
+export default api
