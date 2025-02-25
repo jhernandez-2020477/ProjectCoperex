5e843b4188c988843c5150b2a66047505129be89
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-02-24 23:00:39 -0600

    Se agregan validaciones para usuario, categoria, empresa

--- helpers/validators.js
+++ helpers/validators.js
@@ -36,3 +36,76 @@ export const loginValidation = [
         .isLength({min: 8}),
     validateErrorWithoutImg
 ]
+
+//CATEGORIA
+export const validSaveCate = [
+    body('name', 'Name cannot be empty')
+        .notEmpty() 
+        .isLength({ max: 50 })
+        .withMessage('Can´t be overcome 50 characters'),
+    body('description', 'Description cannot be empty')
+        .notEmpty()
+        .isLength({ max: 150 })
+        .withMessage('Can´t be overcome 150 characters'),       
+        validateErrorWithoutImg
+]
+
+//EMPRESA
+
+export const validSaveCompany = [
+    body('name', 'Name cannot be empty')
+        .notEmpty()
+        .isLength({ max: 25 })
+        .withMessage('Can´t be overcome 25 characters'),
+    body('impactLevel', 'Impact Level can no be empty')
+        .toUpperCase()  
+        .notEmpty()
+        .isIn(['BAJO', 'MEDIO', 'ALTO'])
+        .withMessage('Impact Level must be either "BAJO","MEDIO" or "ALTO"'),
+    body('yearsOfExperience', 'Years Of Experience cannot be empty')
+        .notEmpty()
+        .isLength({ max: 10 })
+        .withMessage('Can´t be overcome 10 characters'),
+    body('businessCategory', 'Business Category cannot be empty')
+        .notEmpty(),
+    body('direction', 'Direction cannot be empty')
+        .notEmpty()
+        .isLength({ max: 100 })
+        .withMessage('Can´t be overcome 100 characters'),
+    body('contact', 'Contact cannot be empty')
+        .notEmpty()
+        .isLength({ min: 8 })
+        .withMessage('Can´t must 8 numbers')
+        .isLength({ max: 13 })
+        .withMessage('Can´t be overcome 13 characters'),
+        validateErrorWithoutImg
+]
+
+export const validUpdateCompany = [
+    body('name', 'Name is Optional')
+        .optional()
+        .isLength({ max: 25 })
+        .withMessage('Can´t be overcome 25 characters'),
+    body('impactLevel', 'Impact Level is Optional')
+        .toUpperCase()  
+        .optional()
+        .isIn(['BAJO', 'MEDIO', 'ALTO'])
+        .withMessage('Impact Level must be either "BAJO","MEDIO" or "ALTO"'),
+    body('yearsOfExperience', 'Years Of Experience is Optional')
+        .optional()
+        .isLength({ max: 10 })
+        .withMessage('Can´t be overcome 10 characters'),
+    body('businessCategory', 'Business Category is Optional')
+        .optional(),
+    body('direction', 'Direction is Optional')
+        .optional()
+        .isLength({ max: 100 })
+        .withMessage('Can´t be overcome 100 characters'),
+    body('contact', 'Contact is Optional')
+        .optional()
+        .isLength({ min: 8 })
+        .withMessage('Can´t must 8 numbers')
+        .isLength({ max: 13 })
+        .withMessage('Can´t be overcome 13 characters'),
+        validateErrorWithoutImg
+]
