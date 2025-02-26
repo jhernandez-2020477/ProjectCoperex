c628162b7f75082e36891cfc69de13419b230e0a
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-02-25 22:40:09 -0600

    Se añaden rutas de el reporte

--- src/report/report.routes.js
+++ src/report/report.routes.js
@@ -1,1 +1,27 @@
 //Rutas de autenticación
+import { Router } from 'express';
+import { generateReport, getAll } from '../report/report.controller.js'
+import { isAdmin, validateJwt } from "../../middlewares/validate.jwt.js"
+
+const api = Router()
+
+// Ruta para generar el reporte
+api.get(
+    '/generateReport',
+    [
+        validateJwt,
+        isAdmin
+    ], 
+    generateReport
+)
+
+api.get(
+    '/getAll',
+    [
+        validateJwt,
+        isAdmin
+    ],
+    getAll
+)
+
+export default api
