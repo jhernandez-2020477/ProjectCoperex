ea779820206dbe58a08c5568dc6f4fad0ecc445a
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-02-25 22:41:47 -0600

    Se agrega rutas del usuario

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,1 +1,40 @@
 //Rutas de autenticación
+import { Router } from "express";
+import { validateJwt } from "../../middlewares/validate.jwt.js";
+import { validUpdatePassword, validUpdateUser } from "../../helpers/validators.js";
+import { deleteUser, updatePassword, updateUser } from "./user.controller.js";
+
+const api = Router()
+//Rutas privadas
+
+//Actualizar Usuario
+api.put(
+    '/:id',
+    [
+        validateJwt,
+        validUpdateUser
+    ],
+    updateUser
+)
+
+//Actualizar contraseña
+api.put(
+    '/password/:id',
+    [
+        validateJwt,
+        validUpdatePassword
+    ],
+    updatePassword
+)
+
+//Eliminar Usuario
+api.delete(
+    '/:id',
+    [
+        validateJwt
+    ],
+    deleteUser
+)
+
+//Exportar
+export default api
