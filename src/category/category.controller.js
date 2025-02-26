030aa73341fd31575bd8a95db8f2fd5060528711
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-02-25 22:34:20 -0600

    Se agrega la categoria por defecto, listar, y editar

--- src/category/category.controller.js
+++ src/category/category.controller.js
@@ -1,5 +1,41 @@
 //Lógica de categoria
 import Category from '../category/category.model.js'
+import Company from '../company/company.model.js'
+
+//Crear categoria por defecto
+export const initCategory = async(req, res) => {
+    try {
+        // Verificar si ya existe una categoria con ese nombre
+        let defaultCate = await Category.findOne(
+            { 
+                name: 'Category Default' 
+            }
+        )
+        
+        if (!defaultCate) {
+            const categoryData = {
+                name: 'Category Default',
+                description: 'Esta es una categoria que engloba todas las demás'
+            }
+
+            let newCategory = new Category(categoryData)
+            await newCategory.save();
+            
+            console.log('Category created successfully!')
+        } else {
+            //console.log('Category already exists!')
+        }
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send(
+            {
+                message: 'Error creating Default Category', 
+                err
+            }
+        )
+    }
+}
+
 
 //Crear Categoria
 export const saveCategory = async(req, res)=>{
@@ -22,6 +58,47 @@ export const saveCategory = async(req, res)=>{
         )
     }
 }
+
+//Editar categoria
+export const updateCategory = async(req, res) => {
+    const { id } = req.params
+    const { ...data } = req.body 
+    try {
+
+        const updateCate = await Category.findByIdAndUpdate(
+            id,
+            data,
+            { new: true }
+        )
+        
+        if (!updateCate) {
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: 'Category not found, not updated'
+                }
+            )
+        }
+
+        return res.send(
+            {
+                success: true,
+                message: 'Category updated successfully :)',
+                updateCate
+            }
+        )
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'General error when adding Category',
+                err
+            }
+        )
+    }
+}
+
 
 //Obtener todas las categorias
 export const getAll = async(req, res)=>{
@@ -59,3 +136,66 @@ export const getAll = async(req, res)=>{
 }
 
 
+//Eliminar categoria
+export const deleteCategory = async(req, res) => {
+    const { id } = req.params
+    
+    try {
+        let defaultCate = await Category.findOne(
+            {
+                name: 'Category Default' 
+            }
+        )
+        
+        if (!defaultCate) {
+            return res.status(404).send(
+                {
+                    message: 'Default category not found'
+                }
+            )
+        }
+
+        // Buscar las Empresas que tienen la categoría a eliminar
+        let companiesToUpdate = await Company.find(
+            { 
+                businessCategory: id 
+            }
+        )
+
+        // Si hay publicaciones, se actualiza su categoría a la categoría por defecto
+        if (companiesToUpdate.length > 0) {
+            await Company.updateMany(
+                { businessCategory: id },
+                { $set: { businessCategory: defaultCate._id } }
+            )
+            console.log(`${companiesToUpdate.length} Company updated to default category`)
+        }
+
+        // Eliminar la categoría
+        let deletedCategory = await Category.findByIdAndDelete(id)
+
+        if (!deletedCategory) {
+            return res.status(404).send(
+                {
+                    message: 'Category not found to delete'
+                }
+            )
+        }
+
+        return res.send(
+            {
+                success: true,
+                message: 'Category deleted successfully, and Companies updated to default category'
+            }
+        )
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'Error deleting category',
+                err
+            }
+        )
+    }
+}
